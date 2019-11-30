5c68af38c34df03adae427c56f359d0a413f98aa
karthik katipally <karthik.katipalli@gmail.com>
2019-11-30 08:10:39 +0530

    removing depthwise since conv can proxy

--- src/lib/nnlib.jl
+++ src/lib/nnlib.jl
@@ -1,5 +1,5 @@
 using NNlib
-import NNlib: softmax, ∇softmax, logsoftmax, ∇logsoftmax, conv, depthwiseconv, ∇conv_data, ∇depthwiseconv_data, maxpool, meanpool, σ, relu, batched_mul, batched_adjoint
+import NNlib: softmax, ∇softmax, logsoftmax, ∇logsoftmax, conv, ∇conv_data, maxpool, meanpool, σ, relu, batched_mul, batched_adjoint
 
 @adjoint function Base.Broadcast.broadcasted(::typeof(relu), x::Numeric)
   relu.(x), Δ -> (nothing, ifelse.(x .> 0, Δ, zero.(x)))
@@ -15,7 +15,7 @@ end
 @adjoint logsoftmax(xs; dims=1) = logsoftmax(xs, dims=dims), Δ -> (∇logsoftmax(Δ, xs, dims=dims),)
 
 @adjoint NNlib.DenseConvDims(args...; kwargs...) = NNlib.DenseConvDims(args...; kwargs...), _ -> nothing
-@adjoint NNlib.DepthwiseConvDims(args...; kwargs...) = NNlib.DepthwiseConvDims(args...; kwargs...), _ -> nothing
+# @adjoint NNlib.DepthwiseConvDims(args...; kwargs...) = NNlib.DepthwiseConvDims(args...; kwargs...), _ -> nothing
 @adjoint NNlib.PoolDims(args...; kwargs...) = NNlib.PoolDims(args...; kwargs...), _ -> nothing
 
 colmajor(x) = colmajor(MemoryLayout(typeof(x)), x)
@@ -45,27 +45,25 @@ colmajor(::AbstractColumnMajor, x) = x
        )
    end
 
-@adjoint depthwiseconv(x, w, cdims; kw...) =
-  depthwiseconv(x, w, cdims; kw...),
-    Δ -> begin
-       Δ = colmajor(Δ)
-       return (
-           NNlib.∇depthwiseconv_data(Δ, w, cdims; kw...),
-           NNlib.∇depthwiseconv_filter(x, Δ, cdims; kw...),
-           nothing,
-       )
-   end
-
-@adjoint ∇depthwiseconv_data(x, w, cdims; kw...) =
-  ∇depthwiseconv_data(x, w, cdims; kw...),
-    Δ -> begin
-       Δ = colmajor(Δ)
-       return (
-           NNlib.depthwiseconv(Δ, w, cdims; kw...),
-           NNlib.∇depthwiseconv_filter(Δ, x, cdims; kw...),
-           nothing,
-       )
-   end
+# @adjoint depthwiseconv(x, w, cdims; kw...) =
+#   depthwiseconv(x, w, cdims; kw...),
+#     Δ -> begin
+#        return (
+#            NNlib.∇depthwiseconv_data(Δ, w, cdims; kw...),
+#            NNlib.∇depthwiseconv_filter(x, Δ, cdims; kw...),
+#            nothing,
+#        )
+#    end
+#
+# @adjoint ∇depthwiseconv_data(x, w, cdims; kw...) =
+#   ∇depthwiseconv_data(x, w, cdims; kw...),
+#     Δ -> begin
+#        return (
+#            NNlib.depthwiseconv(Δ, w, cdims; kw...),
+#            NNlib.∇depthwiseconv_filter(Δ, x, cdims; kw...),
+#            nothing,
+#        )
+#    end
 
 @adjoint function maxpool(x, pdims::NNlib.PoolDims; kw...)
   y = maxpool(x, pdims; kw...)
